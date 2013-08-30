53d2116d6e97286af9b1e42e0ceb8e7fba9e8875
Hongbo Yang <abcrun@gmail.com>
2013-08-30 18:35:05 +0800

    Add projects

---
+++ AutoComplete.js
@@ -0,0 +1,99 @@
+var getPosition = function(elm,pos){
+    var distance = 0;
+    while(elm){
+        distance += elm[pos];
+        elm = elm.offsetParent;
+    }
+    return distance;
+}
+var AutoComplete = Class.create({
+    constructor:function(obj){
+		var temp;
+        var input = 'oninput' in window?'input':'keyup';
+
+        this.tip = obj.tip || '请输入关键字';
+        this.input = obj.input;
+        if(!this.input) return;
+        this.input.val(this.tip);
+
+		this.form = obj.form
+		if(!this.form && (temp = this.input.parents('form')) && temp.length){
+			this.form = temp;
+			temp = null;
+		}
+
+        this.suggest = obj.suggest;
+        if(!this.suggest){
+            var div = document.createElement('div'),x = getPosition(this.input[0],'offsetLeft'),y = getPosition(this.input[0],'offsetTop') + this.input.height();
+            div.style.cssText = 'position:absolute;top:' + y + 'px;left:' + x + 'px';
+            document.body.appendChild(div);
+            this.suggest = jQuery(div);
+        }
+        this.suggest.hide();
+
+        this.list = (obj.list || 'li').toLowerCase();
+        this.hover = obj.hover || 'hover';
+
+        this.input.focus(jQuery.proxy(this.focus,this));
+        this.input.blur(jQuery.proxy(this.blur,this));
+        this.input.keydown(jQuery.proxy(this.keydown,this));
+        this.input.bind(input,jQuery.proxy(this.textInput,this));
+
+		if(this.form) this.form.bind('submit',jQuery.proxy(this.submit,this));
+        this.suggest.delegate(this.list,'mouseover',jQuery.proxy(this.mouseover,this));
+        this.suggest.delegate(this.list,'click',jQuery.proxy(this.click,this));
+    },
+    focus:function(){
+        if(this.input.val() == this.tip) this.input.val('');
+    },
+    blur:function(){
+        var self = this;
+        if(this.input.val() == '') this.input.val(this.tip);
+        setTimeout(function(){
+            self.suggest.hide();
+            self.current = null;
+        },500)
+    },
+    click:function(){
+        this.input.val(this.current.text());
+    },
+    submit:function(evt){
+		if(this.form) evt.preventDefault();
+        this.input.val(this.current.text());
+    },
+    keydown:function(evt){
+        var keyCode = evt.keyCode,tag = this.list,hover = this.hover;
+        if(keyCode >= 37 && keyCode <= 40){
+            if(!this.suggest.find(tag).length) return;
+			evt.preventDefault();
+
+            if(this.current) this.current.removeClass(hover);
+            if(keyCode == 37 || keyCode == 38){
+                this.current = (!this.current?this.suggest.find(tag).last():(this.current.prev().length?this.current.prev():this.suggest.find(tag).last()))
+            }else{
+                this.current = (!this.current?this.suggest.find(tag).first():(this.current.next().length?this.current.next():this.suggest.find(tag).first()))
+            }
+            this.current.addClass(hover);
+			this.input.val(this.current.text());
+        }else if(keyCode == 13){
+            if(!this.form) this.submit();
+        }
+    },
+	getDatas:function(){
+		throw new Error('请在子类添加".getDatas()"这个方法，来用于请求或者输出智能提示的内容');
+	},//Abstract Method - get the prompt
+    textInput:function(evt){
+        var keyCode = evt.keyCode;
+        if(!keyCode || ((keyCode >=48 && keyCode <= 90) || (keyCode >=96 && keyCode <= 105) || keyCode == 8 || keyCode == 32 || keyCode == 45 || keyCode == 13)){
+			this.getData();
+		}
+    },	
+    mouseover:function(evt){
+        var target = evt.target;
+        while(target && target.nodeName.toLowerCase() != this.list) target = target.parentNode;
+		if(target.nodeName.toLowerCase() != this.list) return;
+        if(this.current) this.current.removeClass(this.hover);
+        this.current = jQuery(target);
+        this.current.addClass(this.hover);
+    }
+});
---
+++ Controller.js
@@ -0,0 +1,134 @@
+/**
+* Javascript Controller Module
+* The MIT License - Copyright (c) 2013 Hongbo Yang <abcrun@gmail.com>
+* Repository - https://github.com/abcrun/sack.git
+* Version - 0.0.1
+*/
+(function(name,factory){
+	if(typeof define === 'function' && define.amd) define(factory);//AMD
+	else if(typeof module === 'object' && module.exports) module.exports = factory();//CommonJS
+	else this[name] = factory();//Global
+})('Controller',function(){
+	//Publisher Constructor
+	var Publisher = function(publisher){
+		this.publisher = publisher || this;
+		this.subscribers = [];
+	}
+	Publisher.prototype = {
+		add:function(subscriber){
+			var subscribers = this.subscribers;
+			for(var i = 0; i < subscribers.length; i++){
+				var sub = subscribers[i];
+				if(sub == subscriber) return this;
+			}
+			this.subscribers.push(subscriber);
+			return this;
+		},
+		remove:function(subscriber){
+			var subscribers = this.subscribers;
+			for(var i = 0; i < subscribers.length; i++){
+				var sub = subscribers[i];
+				if(sub == subscriber){
+					this.subscribers.splice(i,1);
+					return this;
+				}
+			}
+		},
+		deliver:function(data){
+			for(var i = 0; i < this.subscribers.length; i++){
+				var subscriber = this.subscribers[i];
+				subscriber.call(this.publisher,data);
+			}
+		}
+	}
+
+	//event listeners
+	var formatEvent = function(event){//format event object
+		event.target = event.target || event.srcElement;
+		event.keyCode = event.keyCode || event.which;
+		event.pageX = event.pageX || document.body.scrollLeft + event.clientX;
+		event.pageY = event.pageY || document.body.scrollTop + event.clientY;
+		event.preventDefault = event.preventDefault || function(){event.returnValue = false}
+		event.stopPropagation = event.stopPropagation || function(){event.cancelBubble = true}
+		return event;
+	}
+	var addListener = function(elm,type,handlers){
+		try{
+			elm.addEventListener(type,handlers,false);
+		}catch(e){
+			try{
+				elm.attachEvent('on' + type,handlers);
+			}catch(e){}
+		}
+	}
+	var removeListener = function(elm,type,handlers){
+		try{
+			elm.removeEventListener(type,handlers,false);
+		}catch(e){
+			try{
+				elm.detachEvent('on' + type,handlers);
+			}catch(e){}
+		}
+	}
+	var events = function(){
+		var setNull = function(elm,type){
+			removeListener(elm,type,elm['events'][type]['handlers'],false);
+			elm['events'][type] = null;
+		}
+		return{
+			bind:function(type,fn){
+				for(var i = 0; i < this.elms.length; i++){
+					var elm = this.elms[i];
+					elm['events'] = elm['events'] || {};
+					if(elm['events'][type]) elm['events'][type].add(fn);
+					else{
+						elm['events'][type] = new Publisher(elm).add(fn);
+						elm['events'][type]['handlers'] = function(event){
+							event = formatEvent(event);
+							elm['events'][type].deliver(event);
+						}
+						addListener(elm,type,elm['events'][type]['handlers']);
+					}
+				}
+				return this;
+			},
+			unbind:function(type,fn){
+				for(var i = 0; i < this.elms.length; i++){
+					var elm = this.elms[i];
+					if(!fn) setNull(elm,type);
+					else{
+						elm['events'][type].remove(fn);
+						if(!elm['events'][type]['subscribers'].length) setNull(elm,type);
+					}
+				}
+				return this;
+			},
+			trigger:function(type){
+				if(!type) return;
+				for(var i = 0; i < this.elms.length; i++){
+					var elm = this.elms[i];
+					elm['events'][type].deliver();
+				}
+				return this;
+			}
+		}
+	}
+
+	var define = function(arg){
+		this.elms = arg.nodeName ? [arg] :
+			arg.length && arg[0].nodeName ? arg : 
+			null;
+	}
+	define.prototype = events();
+	define.prototype.on = events.bind;
+	define.prototype.un = events.unbind;
+
+	var controller = {
+		Publisher:Publisher,
+		define:function(arg){
+			return new define(arg);
+		}
+	}
+
+	return controller;
+})
